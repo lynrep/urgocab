83cc6e3895edf25681cdb1946ff15cc62d3256ef
Ailyn <lynrep@users.noreply.github.com>
2023-10-27 15:09:34 +0200

    added ReadMe

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
   <footer class="mx-auto h-full w-full mt-10">
     <div class="flex justify-between p-4 bg-yellow-100">
-      <p class="ml-10">&copy; 2023 UrGoCab. All rights reserved.</p>
+      <p class="ml-10">
+        <i class="fa-regular fa-copyright"></i>2023 UrGoCab. All rights
+        reserved.
+      </p>
       <div class="space-x-4 mr-10">
         <a href="#" class="hover:text-gray-300">Home</a>
         <a href="#" class="hover:text-gray-300">About</a>
