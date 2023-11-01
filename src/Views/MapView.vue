d6433948eeb12913004a07123239f7df86e22fa9
Ailyn <lynrep@users.noreply.github.com>
2023-11-01 13:52:15 +0100

    added mapView and routerlink on brandname

---
+++ src/Views/MapView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="mapView" class="flex flex-col h-screen bg-gray-300">
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  setTimeout(() => {
+    initMap();
+  }, 50);
+});
+
+const initMap = () => {
+  const map = tt.map({
+    key: "DH91sqcc9iFTpWTV7XDyZQ1mf1tT92LY",
+    container: "map",
+    dragPan: !isMobileOrTablet(),
+  });
+
+  map.addControl(new tt.FullscreenControl());
+  map.addControl(new tt.NavigationControl());
+
+  console.log(map);
+};
+</script>
+
+<style>
+#MapView {
+  #map {
+    width: 100%;
+    height: 45vh;
+    top: 0px;
+    left: 0px;
+  }
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,21 @@
-import HomeView from './Views/HomeView.vue'
-import DirectionsView from './Views/DirectionsView.vue'
+import HomeView from "./Views/HomeView.vue";
+import DirectionsView from "./Views/DirectionsView.vue";
+import MapView from "./Views/MapView.vue";
 
 export const routes = [
-    
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-      },
-      {
-        path: '/directions',
-        name: 'directions',
-        component: DirectionsView
-      },
-
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/directions",
+    name: "directions",
+    component: DirectionsView,
+  },
+  {
+    path: "/map",
+    name: "map",
+    component: MapView,
+  },
 ];
-
