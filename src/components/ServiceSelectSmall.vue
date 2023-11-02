cfa48eacd8426806a2d09339ee7efaae0ec55290
Ailyn <lynrep@users.noreply.github.com>
2023-11-02 11:13:15 +0100

    added serviceselectsmall styles,not finished

--- src/components/ServiceSelectSmall.vue
+++ src/components/ServiceSelectSmall.vue
@@ -1,20 +1,18 @@
 <template>
-  <section>
-    <div id="serviceSelectSmall">
-      <div class="relative w-full rounded-2xl bg-white h-20 text-xl font-bold">
-        <!--main features-->
+  <div id="serviceSelectSmall">
+    <div class="relative w-full rounded-2xl bg-white h-20 text-xl font-bold">
+      <!--main features-->
 
-        <div class="flex justify-between p-2">
-          <div class="text-xs text-center text-gray-900 mt-2">
-            {{ text }}
-          </div>
-          <div class="mt-3 mr-1">
-            <img :width="55" src="/Img/Journey.png" alt="" />
-          </div>
-        </div>
+      <div
+        class="absolute top-1/3 left-1/3 transform -translate-x-1/4 -translate-y-1/4 mt-0.5"
+      >
+        <img :width="55" src="/Img/Journey.png" alt="" />
       </div>
     </div>
-  </section>
+    <div class="text-m text-center text-gray-900 mt-2">
+      {{ text }}
+    </div>
+  </div>
 </template>
 
 <script>
