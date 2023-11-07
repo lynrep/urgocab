711e753fa5dcafac05b5f6757e77ca11d726c36b
Ailyn <lynrep@users.noreply.github.com>
2023-11-07 20:24:56 +0100

    fixing directionsView

--- src/Views/DirectionsView.vue
+++ src/Views/DirectionsView.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="Directions" class="flex flex-col h-screen bg-indigo-800">
     <!-- new!!!!!!!!! -->
-    <div class="grid grid-cols-12 mt-4 shadow-md  ">
-      <div class="col-span-2 mx-auto  ">
+    <div class="grid grid-cols-12 mt-4 shadow-md">
+      <div class="col-span-2 mx-auto">
         <div>
           <RouterLink :to="{ name: 'home' }">
-          <ArrowIcon :size="35" />
+            <ArrowIcon :size="35" />
           </RouterLink>
-
         </div>
-
-        
 
         <div class="h-16 mt-4 w-full">
-          <div :class="[isPickupActive ? 'circle-black' : 'circle-gray']" />
+          <div :class="[isPickupActive ? 'circle-black' : 'circle-gray']"></div>
           <div class="line"></div>
-          <div :class="[!isPickupActive ? 'square-black' : 'square-gray']" />
+          <div
+            :class="[!isPickupActive ? 'square-black' : 'square-gray']"
+          ></div>
         </div>
       </div>
-      <div class="col-span-1o pr-4 ">
+      <div class="col-span-10 pr-4">
         <div class="w-full h-5"></div>
         <div class="mb-2 mt-5">
           <AutoCompleteInput
@@ -26,28 +25,28 @@
             v-model:input="pickup"
             placeholder="Enter pick-up location"
             @clearInput="clearInputFunc('firstInput')"
-            @isActive="isPickupActive = true"
+            @isActive="isPickupActive"
           />
         </div>
 
         <div class="mb-3">
           <AutoCompleteInput
-            theId="firstInput"
+            theId="secondInput"
             v-model:input="pickup"
             placeholder="Where to?"
             @clearInput="clearInputFunc('firstInput')"
-            @isActive="isPickupActive = true"
+            @isActive="!isPickupActive"
           />
         </div>
       </div>
     </div>
-    <div class="flex items-center costom-border-bottom bg-gray-200 ">
-      <div class="bg-gray-400 mx-5 my-3.5 p-1.5 rounded-full ">
-          <MapMarkerIcon :size="30" fillcolor="#f5f5f5" />
+    <div class="flex items-center costom-border-bottom bg-gray-200">
+      <div class="bg-gray-400 mx-5 my-3.5 p-1.5 rounded-full">
+        <MapMarkerIcon :size="30" fillcolor="#f5f5f5" />
       </div>
       <div>
-          <div class="text-lg text-gray-600">Barcelona, Spain</div>
-          <div class="text-lg text-gray-400">Spain</div>
+        <div class="text-lg text-gray-600">Barcelona, Spain</div>
+        <div class="text-lg text-gray-400">Spain</div>
       </div>
     </div>
 
@@ -59,13 +58,13 @@
 //  New!!!!!!!!!!!!!!!
 import AutoCompleteInput from "../components/AutoCompleteInput.vue";
 import ArrowIcon from "vue-material-design-icons/ArrowLeft.vue";
-import MapMarkerIcon from 'vue-material-design-icons/MapMarker.vue';
+import MapMarkerIcon from "vue-material-design-icons/MapMarker.vue";
 
 export default {
   components: {
     ArrowIcon,
     AutoCompleteInput,
-    MapMarkerIcon, 
+    MapMarkerIcon,
   },
   data() {
     return {
@@ -79,41 +78,43 @@ export default {
 
 <style>
 /* new!!!!!!!!!!!!! */
-#Directions .circle-black {
-  margin: 0 auto;
-  width: 9px;
-  height: 9px;
-  background-color: black;
-  border-radius: 100%;
-}
+#Directions {
+  .circle-black {
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: black;
+    border-radius: 100%;
+  }
 
-.circle-gray {
-  margin: 0 auto;
-  width: 9px;
-  height: 9px;
-  background-color: rgb(191, 191, 191);
-  border-radius: 100%;
-}
+  .circle-gray {
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: rgb(191, 191, 191);
+    border-radius: 100%;
+  }
 
-.square-black {
-  margin: 0 auto;
-  width: 9px;
-  height: 9px;
-  background-color: black;
-}
+  .square-black {
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: black;
+  }
 
-.square-gray {
-  margin: 0 auto;
-  width: 9px;
-  height: 9px;
-  background-color: rgb(191, 191, 191);
-}
+  .square-gray {
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: rgb(191, 191, 191);
+  }
 
-.line {
-  margin: 0 auto;
-  width: 2px;
-  height: 45px;
-  background-color: rgb(191, 191, 191);
+  .line {
+    margin: 0 auto;
+    width: 2px;
+    height: 45px;
+    background-color: rgb(191, 191, 191);
+  }
 }
 /* new!!!!!!!!!!!!! */
 </style>
--- src/components/AutoCompleteInput.vue
+++ src/components/AutoCompleteInput.vue
@@ -1,66 +1,48 @@
 <template>
-   <div id="AutoCompleteInput" class="flex items-center">
-    <input 
-        :id="theId"
-        v-model="inputComputed"
-        @click="isActive = true"
-        class="
-            text-md
-            bg-gray-100
-            appearance-none 
-            rounded 
-            w-96  
-            py-2.5
-            px-3
-            text-gray-700 
-            leading-tight 
-            focus:outline-none 
-            focus:shadow-outline
-            focus:bg-gray-200
-            "   
-            type="text"
-            placeholder="placeholder"
-            autocomplete="off"
-    >
-      <WindowCloseIcon 
-      fillColor="#2e2e2d" 
-      @click="clearInput" 
-      />
-   </div>
+  <div id="AutoCompleteInput" class="flex items-center">
+    <input
+      :id="theId"
+      v-model="inputComputed"
+      @click="emit('isActive', 'true')"
+      class="text-md bg-gray-100 appearance-none rounded w-96 py-2.5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-200"
+      type="text"
+      :placeholder="placeholder"
+      autocomplete="off"
+    />
+    <WindowCloseIcon fillColor="#2e2e2d" @click="clearInput" />
+  </div>
 </template>
 
 <script>
-import { computed, defineEmits, defineProps, toRefs } from 'vue';
-import WindowCloseIcon from 'vue-material-design-icons/WindowClose.vue';
+import { computed, defineEmits, defineProps, toRefs } from "vue";
+import WindowCloseIcon from "vue-material-design-icons/WindowClose.vue";
 
 export default {
   components: {
-    WindowCloseIcon
+    WindowCloseIcon,
   },
   props: {
     theId: String,
     input: String,
-    placeholder: String
+    placeholder: String,
   },
   setup(props) {
-    const emit = defineEmits(['update:input', 'clearInput', 'isActive']);
+    const emit = defineEmits(["update:input", "clearInput", "isActive"]);
 
     const { theId, input, placeholder } = toRefs(props);
 
     const inputComputed = computed({
       get: () => input.value,
-      set: (val) => emit('update:input', val)
+      set: (val) => emit("update:input", val),
     });
 
     return {
       theId,
       input,
       placeholder,
-      inputComputed
+      inputComputed,
     };
-  }
-}
+  },
+};
 </script>
-<style>
-  
-</style>
+<style></style>
